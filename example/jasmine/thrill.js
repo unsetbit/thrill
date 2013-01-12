0fa16c202a42526d3ba03c2eb6628f21f5804ed1
Ozan Turgut <ozanturgut@gmail.com>
2013-01-11 23:37:36 -0800

    Jasmine integration
    Signed-off-by: Ozan Turgut <ozanturgut@gmail.com>

---
+++ example/jasmine/thrill.js
@@ -0,0 +1,11 @@
+var serve = {
+	'thrill-jasmine-adapter.js': '../../dist/thrill-jasmine-adapter.js',
+	'': '../../../jasmine'
+};
+
+var run = 'spec/runner.html';
+
+host = "192.168.0.100";
+timeout = 1000 * 1000;
+//verbose = true;
+//quiet = true;
